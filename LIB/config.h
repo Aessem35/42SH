b267388d09e017ae073a47333d952e77cbfc9243
loteri_a <loteri_a@epitech.eu>
2014-04-23 19:28:13 +0200

    From the beggining

---
+++ LIB/config.h
@@ -0,0 +1,16 @@
+/*
+** config.h for 42sh in /home/loteri_a/rendu/42sh/42SH/LIB
+** 
+** Made by 
+** Login   <loteri_a@epitech.net>
+** 
+** Started on  Wed Apr 23 19:25:15 2014 loteri_a
+** Last update Wed Apr 23 19:26:26 2014 
+*/
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define PROMPT "42sh> "
+
+#endif
---
+++ SRC/42sh.c
@@ -0,0 +1,18 @@
+/*
+** 42sh.c for 42sh in /home/loteri_a/rendu/42sh/42SH/SRC
+** 
+** Made by 
+** Login   <loteri_a@epitech.net>
+** 
+** Started on  Wed Apr 23 19:26:49 2014 
+** Last update Wed Apr 23 19:27:49 2014 
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "../LIB/config.h"
+
+int	main(int ac, char **av)
+{
+}
